6c738a57c40a3a592ef6a55209ad09e35e68e274
the-vansh <Contactvanshsaini@gmail.com>
2025-06-29 23:13:28 +0530

    first commit

---
+++ src/main/java/com/Vansh/Online/Learning/App/OnlineLearningAppApplication.java
@@ -0,0 +1,13 @@
+package com.Vansh.Online.Learning.App;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineLearningAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineLearningAppApplication.class, args);
+	}
+
+}
