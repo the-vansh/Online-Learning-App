5e098c919f1c4e3f62a8d8307b068874eb8ef761
the-vansh <Contactvanshsaini@gmail.com>
2025-07-14 22:02:56 +0530

    All set

--- src/main/java/com/Vansh/Online/Learning/App/Configuration/CloudinaryConfig.java
+++ src/main/java/com/Vansh/Online/Learning/App/Configuration/CloudinaryConfig.java
@@ -2,18 +2,28 @@ package com.Vansh.Online.Learning.App.Configuration;
 
 import com.cloudinary.Cloudinary;
 import com.cloudinary.utils.ObjectUtils;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class CloudinaryConfig {
 
+    @Value("${cloudinary.cloud_name}")
+    private String cloudName;
+
+    @Value("${cloudinary.api_key}")
+    private String apiKey;
+
+    @Value("${cloudinary.api_secret}")
+    private String apiSecret;
+
     @Bean
     public Cloudinary cloudinary() {
         return new Cloudinary(ObjectUtils.asMap(
-                "cloud_name", "djoatyv4p", // Replace with your Cloud name
-                "api_key", "112849827557621",
-                "api_secret", "YYE6qXSl4jQZAl2eP7cgj1cDLo4"
+                "cloud_name", cloudName,
+                "api_key", apiKey,
+                "api_secret", apiSecret
         ));
     }
 }
